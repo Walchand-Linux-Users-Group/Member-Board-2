f509c96c635ae2660c77e358056531d70880b210
thundersp <thundersp20504@gmail.com>
2024-03-27 15:00:44 +0530

    Updated the line and Resolved tux cut

--- src/Components/Information/Info.jsx
+++ src/Components/Information/Info.jsx
@@ -5,31 +5,18 @@ export default function Info() {
   return (
     <>
       <div
-        className="infocon container mx-auto p-4 m-4 flex flex-col
+        className="infocon container mx-auto flex flex-col
       justify-center items-center "
       >
-        <p className="infol text-4xl md:text-5xl font-bold dark:text-white text-center">
-          {" "}
+        <p className="infol text-4xl md:text-5xl font-bold dark:text-white text-center mt-2">
           Member Board Drive 2
         </p>
-        {/* <p className="ml
-            text-1xl md:text-3xl font-bold ">Last Chance to be a part of this family</p>
-            <p className="sl">Apply now</p> */}
-        {/* <SparklesPreview/> */}
-
-        {/*  
-            
-                Passed all text contents to this DEMO comp -> 
-                couldve done better using Props or Coustom Hooks 
-                but did anyways sorry 
-                                                --Vighnesh 1:13 mar23
-            */}
-        <p className="infopara font-bold px-2 ">
-          {" "}
-          Join our Linux community. Experience Open Source.
+        <p className="infopara">
+          Prepare to join a vibrant community of Linux enthusiasts and immerse
+          yourself in the dynamic world of Open Source
         </p>
         <div className="text-center text-xl mx-auto">
-          <div className="typewriter-container p-4 mx-auto">
+          <div className="typewriter-container mx-auto">
             <TypewriterEffectSmoothDemo />
           </div>
         </div>
